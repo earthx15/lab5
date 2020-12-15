3081571353ee2f8cb41fc4bc237c0db397241fb5
earthx15 <uncles1512@hotmail.com>
2020-12-15 17:03:30 +0700

    "lab5_2.cpp completed"

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -17,8 +17,10 @@ double pythagoras(double xcomp,double ycomp){
     return sqrt(pow(xcomp,2)+pow(ycomp,2));
 }
 void  showResult(double result_vec_length,double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
     cout << "Length of the resultant vector = " << result_vec_length << "\n";
     cout << "Direction of the resultant vector (deg) = " << result_vec_direction<<"\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
 } 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -37,7 +39,5 @@ int main(){
     ycomp = findYComponent(l1,l2,a1,a2);
     result_vec_length = pythagoras(xcomp,ycomp);
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
-    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
     showResult(result_vec_length,result_vec_direction);
-    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
 }
